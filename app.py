0f56a6932e86c55039652f36dce2c72d850ab7fc
Matt Stiller <matthewstiller484@yahoo.com>
2022-06-15 16:46:49 -0400

    Incorporated templates. #TODO template inheritance.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 app.config.from_pyfile('settings.py')
@@ -6,7 +6,7 @@ app.config.from_pyfile('settings.py')
 @app.route('/')
 @app.route('/home')
 def index():
-    return "Whatever you want here."
+    return render_template('basic.html')
 
 if __name__ == '__main__':
     app.run()
